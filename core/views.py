9a99ee1badc13f6ecb4f15f22c61ba5af72c6c1a
Nhật Hồng <nhathong@192.168.1.9.non-exists.ptr.local>
2025-02-26 16:53:09 +0700

    update

--- core/views.py
+++ core/views.py
@@ -64,3 +64,7 @@ def ajax_contact_form(request):
         "success": True,
         "message": " Message sent successfully."
     })
+
+
+def error(request, exception):
+    return render(request, "error.html", {"message": exception})
--- rebound/settings.py
+++ rebound/settings.py
@@ -31,7 +31,7 @@ env.read_env(str(BASE_DIR / ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
