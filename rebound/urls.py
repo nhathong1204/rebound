915f5e836739a4e929c9888047080584b67c17f4
Nhật Hồng <nhathong@MacBook-Pro---Nhat-Hong.local>
2025-03-21 09:46:27 +0700

    Update rebound/urls.py

--- rebound/urls.py
+++ rebound/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
 
 urlpatterns = i18n_patterns(
     path("i18n/", include("django.conf.urls.i18n")),
     path("admin/", admin.site.urls),
+    re_path(r"^admin$", RedirectView.as_view(url='/admin/', permanent=True)),
     path("rosetta/", include("rosetta.urls")),
     path("user/", include("userauths.urls")),
     path("", include("core.urls")),
