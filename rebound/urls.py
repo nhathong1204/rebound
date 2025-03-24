6ed0bc5364801a0d158044254706427ebcb735fb
Nhật Hồng <nhathong@192.168.1.13.non-exists.ptr.local>
2025-03-24 17:51:31 +0700

    Update rebound/urls.py

--- rebound/urls.py
+++ rebound/urls.py
@@ -2,8 +2,7 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.generic.base import RedirectView
+from django.urls import include, path
 
 
 urlpatterns = i18n_patterns(
